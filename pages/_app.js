0770690ffb75a44825c7943ce31d7c2f1b578c5f
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-06-28 17:56:51 +0200

    get portfolio by id

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/main.scss";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} appProps="Hello From App" />;
+  return <Component {...pageProps} />;
 };
 
 export default App;
